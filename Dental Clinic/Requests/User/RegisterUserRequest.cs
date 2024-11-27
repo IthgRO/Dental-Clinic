c4b1bd79e6949d20a9bb9c94610eeeec235ef81f
Pentaflouride <timoteiturculetu@gmail.com>
2024-11-27 16:10:52 +0200

    Added rules for registration

--- Dental Clinic/Requests/User/RegisterUserRequest.cs
+++ Dental Clinic/Requests/User/RegisterUserRequest.cs
@@ -30,8 +30,33 @@ namespace Dental_Clinic.Requests.User
         public RegisterUserRequestAbstractValidator() 
         {
             RuleFor(x => x.FirstName)
-                .MaximumLength(10)
-                .WithMessage("First name cannot be longer than 10 characters! ");     
+                .MaximumLength(20)
+                .WithMessage("First name cannot be longer than 20 characters! ");
+
+            RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name is required")
+            .MaximumLength(20).WithMessage("Last name cannot exceed 20 characters");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Invalid email format");
+            
+            RuleFor(x => x.Phone)
+                .NotEmpty().WithMessage("Phone Number is required")
+                .MaximumLength(15).WithMessage("Invalid phone format");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required")
+                .MinimumLength(8).WithMessage("Password must be at least 8 characters")
+                .Matches("[A-Z]").WithMessage("Password must contain an uppercase letter")
+                .Matches("[a-z]").WithMessage("Password must contain a lowercase letter")
+                .Matches("[0-9]").WithMessage("Password must contain a number");  
+
+            RuleFor(x => x.Role)
+                .IsInEnum()
+                .WithMessage("Role must be one of the predefined values: Patient, Dentist, Admin");   
         }
+        
+
     }
 }
